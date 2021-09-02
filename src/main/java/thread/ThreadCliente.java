95b86c52742cdcb52e64051b3e724dfc7338f5b6
Matheus Nasciento <matheusnascimentoti99@gmail.com>
2021-09-02 00:37:11 -0300

    Check get

--- src/main/java/thread/ThreadCliente.java
+++ src/main/java/thread/ThreadCliente.java
@@ -1,9 +1,14 @@
 package thread;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 
 public class ThreadCliente extends Thread {
@@ -16,12 +21,14 @@ public class ThreadCliente extends Thread {
 
     public void run() {
         try {
-            ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
-            out.writeChars("HTTP/1.1 200 OK\r\n"
+
+            Writer out = new OutputStreamWriter(cliente.getOutputStream(), "utf-8");
+            out.write("HTTP/1.1 200 OK\r\n"
                     + "Content-Type: application/json\r\n"
                     + "\r\n"
                     + "hello world");
             out.flush();
+            
             BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
             
             String inputLine;
