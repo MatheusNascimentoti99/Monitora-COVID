d7d4736a0bba818761b64f261ef1db03788a3ff6
Matheus Nasciento <matheusnascimentoti99@gmail.com>
2021-09-02 00:32:54 -0300

    Check get

--- src/main/java/thread/ThreadCliente.java
+++ src/main/java/thread/ThreadCliente.java
@@ -1,14 +1,9 @@
 package thread;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 
 public class ThreadCliente extends Thread {
@@ -21,23 +16,20 @@ public class ThreadCliente extends Thread {
 
     public void run() {
         try {
-
-            Writer out = new OutputStreamWriter(cliente.getOutputStream(), "utf-8");
-            out.write("HTTP/1.1 200 OK\r\n"
+            ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
+            out.writeChars("HTTP/1.1 200 OK\r\n"
                     + "Content-Type: application/json\r\n"
                     + "\r\n"
                     + "hello world");
             out.flush();
-            BufferedReader ois = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            
             String inputLine;
-            sleep(200);
-            while ((inputLine = ois.readLine()) != null) {
+            while ((inputLine = in.readLine()) != null) {
                 System.out.println(inputLine);
-                System.out.println(cliente.getInputStream().available());
-                if(cliente.getInputStream().available() <= 0)
-                    break;
             }
             out.close();
+            in.close();
             cliente.close();
         } catch (Exception e) {
         }
