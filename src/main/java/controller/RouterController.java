96e87dd99d316e25c8c26ef8a3bc7526f14f8ae8
Matheus Nasciento <matheusnascimentoti99@gmail.com>
2021-09-07 12:51:53 -0300

    :sparkles: :hammer: Add CRUD patiente

--- src/main/java/controller/RouterController.java
+++ src/main/java/controller/RouterController.java
@@ -5,27 +5,64 @@
  */
 package controller;
 
+import com.google.gson.Gson;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.regex.Pattern;
 import routes.Router;
-import routes.RouterListUsers;
-import routes.RouterSendForm;
+import routes.MedicoRouter;
+import routes.PacienteRouter;
 
 /**
  *
  * @author matheusnascimento
  */
 public class RouterController {
+
     private Router route;
-    
-    public Object[] router(String path, String method, String body, HashMap data_base){
-        if(path.equals("/paciente/reportClinical") && method.equals("POST")){
-            route = new RouterSendForm();
-            return route.response(body, data_base);
+
+    public Object[] router(String url, String method, String body, HashMap data_base) {
+        String[] urlSplit = url.split("\\?");
+        String path = urlSplit[0];
+
+        HashMap<String, String> params = new HashMap();
+        if (urlSplit.length == 2) {
+            urlSplit = urlSplit[1].split("\\&");
+
+            for (int i = 0; i < urlSplit.length; i++) {
+                String[] entry = urlSplit[i].split("=");
+                params.put(entry[0], entry[1]);
+            }
         }
-        if(path.equals("/medico/pacientes") && method.equals("GET")){
-            route = new RouterListUsers();
-            System.out.println("Listagem");
-            return route.response(body, data_base);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(params);
+        if (path.equals("/paciente")) {
+            route = new PacienteRouter();
+            if (method.equals("POST")) {
+                return route.POST(body, data_base);
+            }
+
+            if (method.equals("PUT")) {
+                return route.PUT(body, data_base);
+            }
+
+            if (method.equals("GET")) {
+                return route.GET(json, data_base);
+            }
+
+            if (method.equals("DELETE")) {
+                return route.DELETE(json, data_base);
+            }
+
+        }
+        if (path.equals("/medico/pacientes") && method.equals("GET")) {
+            route = new MedicoRouter();
+            if (method.equals("GET")) {
+                return route.GET(body, data_base);
+            }
+
         }
         return null;
     }
